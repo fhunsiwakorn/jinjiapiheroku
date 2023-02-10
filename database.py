ec243bc48c9b5c027a13b13da8cf24cdd8c82d27
Siwakorn Banluesapy <siwakorn167@gmail.com>
2023-02-11 00:45:06 +0700

    new change

--- database.py
+++ database.py
@@ -3,9 +3,10 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 # SQLALCHEMY_DATABASE_URL = "mysql+mysqldb://root:@localhost:3306/jinjidb"
-SQLALCHEMY_DATABASE_URL = "mysql://lsbhpub0pbnz1z7r:fcy5et96ohr2mb5j@m7az7525jg6ygibs.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/yh0oupvbrkee4mi2"
-engine = create_engine(SQLALCHEMY_DATABASE_URL,  pool_size=100,
-                       max_overflow=15, pool_pre_ping=True, pool_recycle=60*60)
+SQLALCHEMY_DATABASE_URL = "mysql://vwlz3iy8y3hto1hm:xvnw1o07t6i32px2@m7az7525jg6ygibs.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/bcl3dk8hy73950vs"
+engine = create_engine(SQLALCHEMY_DATABASE_URL,  pool_size=100)
+
+
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
